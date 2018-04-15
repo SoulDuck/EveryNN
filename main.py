7ec95e8ff725eb44a05d6fc958f90f2e8360a777
seongjungkim <plznw4me@naver.com>
2018-04-16 01:11:57 +0900

    add VGG

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ test_imgs=tester.pipeline.test_imgs
 test_labs=tester.pipeline.test_labs
 batch_size = 60
 global_step=0
-global_step = trainer.training(10,global_step ,batch_size)
-tester.validate(test_imgs, test_labs , batch_size ,global_step)
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print 'Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
 
 
 
