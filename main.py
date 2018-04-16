31aeeb544138607479ff72a9d2ed899a37059f54
seongjungkim <plznw4me@naver.com>
2018-04-16 17:47:10 +0900

    add Resnet

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Recorder import Recorder
 from Trainer import Trainer
 from Tester import Tester
 model_name = 'vgg_11'
-vgg = VGG(model_name, True, logit_type='fc' , datatype='cifar10')
+vgg = VGG('sgd' , True , True,   model_name, 'fc'  , 'cifar10')
 recorder = Recorder(folder_name=model_name)
 trainer = Trainer(recorder)
 tester=Tester(recorder)
