0bec24b7aa8adaa186f5516bf1e2a62538f6c26b
seongjungkim <plznw4me@naver.com>
2018-04-17 11:32:35 +0900

    add Resnet 18 , 34 ,50 , 101 , 152

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ model_name = 'vgg_11'
 vgg = VGG('sgd' , True , True,   model_name, 'fc'  , 'cifar10')
 """
 
-model_name = 'resnet_18'
+model_name = 'resnet_36'
 resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
 
 
@@ -29,3 +29,106 @@ for i in range(10):
     global_step = trainer.training(1000,global_step ,batch_size)
 
 tf.reset_default_graph()
+
+
+
+
+model_name = 'resnet_36'
+resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
+
+
+recorder = Recorder(folder_name=model_name)
+trainer = Trainer(recorder)
+tester=Tester(recorder)
+test_imgs=tester.pipeline.test_imgs
+test_labs=tester.pipeline.test_labs
+batch_size = 60
+global_step=0
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print '\t Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
+tf.reset_default_graph()
+
+
+model_name = 'resnet_36'
+resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
+
+
+recorder = Recorder(folder_name=model_name)
+trainer = Trainer(recorder)
+tester=Tester(recorder)
+test_imgs=tester.pipeline.test_imgs
+test_labs=tester.pipeline.test_labs
+batch_size = 60
+global_step=0
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print '\t Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
+tf.reset_default_graph()
+
+
+model_name = 'resnet_50'
+resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
+
+
+recorder = Recorder(folder_name=model_name)
+trainer = Trainer(recorder)
+tester=Tester(recorder)
+test_imgs=tester.pipeline.test_imgs
+test_labs=tester.pipeline.test_labs
+batch_size = 60
+global_step=0
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print '\t Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
+tf.reset_default_graph()
+
+
+model_name = 'resnet_101'
+resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
+
+
+recorder = Recorder(folder_name=model_name)
+trainer = Trainer(recorder)
+tester=Tester(recorder)
+test_imgs=tester.pipeline.test_imgs
+test_labs=tester.pipeline.test_labs
+batch_size = 60
+global_step=0
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print '\t Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
+tf.reset_default_graph()
+
+
+model_name = 'resnet_152'
+resnet_v1 = RESNET_V1('sgd' , True , True , model_name , 'gap' , 'cifar_10')
+
+
+recorder = Recorder(folder_name=model_name)
+trainer = Trainer(recorder)
+tester=Tester(recorder)
+test_imgs=tester.pipeline.test_imgs
+test_labs=tester.pipeline.test_labs
+batch_size = 60
+global_step=0
+
+for i in range(10):
+    mean_acc, mean_loss, pred_all=tester.validate(test_imgs, test_labs , batch_size ,global_step)
+    print '\t Acc :{}  Loss :{} '.format(mean_acc , mean_loss)
+    global_step = trainer.training(1000,global_step ,batch_size)
+
+tf.reset_default_graph()
+
