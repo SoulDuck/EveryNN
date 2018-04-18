3096ae0fbecc060f9d8df1ac50e68bb42bcd5fb4
seongjungkim <plznw4me@naver.com>
2018-04-18 14:10:50 +0900

    add TFRecord Kaggle

--- Dataprovider.py
+++ Dataprovider.py
@@ -229,9 +229,9 @@ class Dataprovider():
             print 'length of filenames : ', len(ret_filename_list)
         return ret_img, ret_lab, ret_filename_list
     @classmethod
-    def get_shuffled_batch(cls , tfrecord_path, batch_size, resize , num_epoch):
+    def get_shuffled_batch(cls , tfrecord_paths, batch_size, resize , num_epoch):
         resize_height, resize_width = resize
-        filename_queue = tf.train.string_input_producer([tfrecord_path], num_epochs=num_epoch , name='filename_queue')
+        filename_queue = tf.train.string_input_producer(tfrecord_paths, num_epochs=num_epoch , name='filename_queue')
         reader = tf.TFRecordReader()
         _, serialized_example = reader.read(filename_queue)
         features = tf.parse_single_example(serialized_example,
