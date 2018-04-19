020ffe5b40135032758c1f4ae58b60c955e2e134
seongjungkim <plznw4me@naver.com>
2018-04-19 12:34:47 +0900

    add TFRecord Kaggle

--- tmp.py
+++ tmp.py
@@ -1,4 +1,13 @@
 f=open('trainLabels.csv' , 'r')
 lines=f.readlines()
+count_0 , count_1 , count_2 , count_3 =0,0,0,0
 for line in lines:
-    print line.split(',')[-1]
+    if line.split(',')[-1] ==0:
+        count_0+=1
+    elif line.split(',')[-1] ==1:
+        count_1 += 1
+    elif line.split(',')[-1] == 2:
+        count_2 += 1
+    elif line.split(',')[-1] == 3:
+        count_3 += 1
+print count_0 ,count_1,count_2,count_3
