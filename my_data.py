679656080bbaa917a7913f92eb963f2be219b0ac
seongjungkim <plznw4me@naver.com>
2018-04-23 12:42:33 +0900

    add TFRecord Kaggle

--- my_data.py
+++ my_data.py
@@ -1,21 +1,26 @@
 import tensorflow as tf
 import Dataprovider
 import numpy as np
-train_normal_tfrecords = './my_data/normal_train.tfrecord'
-train_abnormal_tfrecords = './my_data/abnormal_train.tfrecord'
-test_normal_tfrecords = './my_data/normal_test.tfrecord'
-test_abnormal_tfrecords = './my_data/abnormal_test.tfrecord'
+train_normal_tfrecord = './my_data/normal_train.tfrecord'
+train_abnormal_tfrecord = './my_data/abnormal_train.tfrecord'
+test_normal_tfrecord = './my_data/normal_test.tfrecord'
+test_abnormal_tfrecord = './my_data/abnormal_test.tfrecord'
 
 
-train_tfrecords=[train_normal_tfrecords  ]
-test_tfrecords = [test_normal_tfrecords , test_abnormal_tfrecords]
+train_tfrecords=[train_normal_tfrecord  ]
+test_tfrecords = [test_normal_tfrecord , test_abnormal_tfrecord]
 
 
 
 if '__main__' == __name__:
+    images , labels , fnames=Dataprovider.Dataprovider.reconstruct_tfrecord_rawdata(test_normal_tfrecord)
+    print np.shpae(images)
 
+    """
     images, labels, filenames = Dataprovider.Dataprovider.get_shuffled_batch(tfrecord_paths=train_tfrecords,
                                                                              batch_size=10 , resize=(300,300) , num_epoch=10)
+    
+    
 
     sess=tf.Session()
     coord=tf.train.Coordinator()
@@ -29,3 +34,4 @@ if '__main__' == __name__:
 
 
     sess.close()
+    """
