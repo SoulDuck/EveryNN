f05426197cf2da4d35462bd80f9f695d5c195129
seongjungkim <plznw4me@naver.com>
2018-04-23 23:43:50 +0900

    add TFRecord Kaggle

--- my_data.py
+++ my_data.py
@@ -7,18 +7,14 @@ test_normal_tfrecord = './my_data/tfrecord_normal_0_10_abnormal_100_inf/normal_t
 test_abnormal_tfrecord = './my_data/tfrecord_normal_0_10_abnormal_100_inf/abnormal_test.tfrecord'
 
 
-train_tfrecords=[train_normal_tfrecord ]
+train_tfrecords=[train_normal_tfrecord]
 test_tfrecords = [test_normal_tfrecord , test_abnormal_tfrecord]
-
-
 
 if '__main__' == __name__:
     #images , labels , fnames=Dataprovider.Dataprovider.reconstruct_tfrecord_rawdata(test_normal_tfrecord)
     #print np.shape(images[0])
     #print labels
     #print fnames
-
-
     images, labels, filenames = Dataprovider.Dataprovider.get_shuffled_batch(tfrecord_paths=[test_normal_tfrecord],
                                                                              batch_size=10 , resize=(300,300) , num_epoch=10)
     sess=tf.Session()
@@ -27,4 +23,5 @@ if '__main__' == __name__:
     coord = tf.train.Coordinator()
     tf.train.start_queue_runners(sess=sess, coord=coord)
     images=sess.run(images)
+    print np.shape(images)
     sess.close()
