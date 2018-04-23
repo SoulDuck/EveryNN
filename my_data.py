459820aeec4916d54370ed7f5c3f4c51f06adca2
seongjungkim <plznw4me@naver.com>
2018-04-23 23:37:04 +0900

    add TFRecord Kaggle

--- my_data.py
+++ my_data.py
@@ -29,6 +29,7 @@ if '__main__' == __name__:
     sess.run(init)
     coord = tf.train.Coordinator()
     tf.train.start_queue_runners(sess=sess, coord=coord)
+
     images=sess.run(images)
     print np.shape(images)
 
