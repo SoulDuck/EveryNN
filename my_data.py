756c61902f6e2d24e8368262876ebc4e7656c2b1
seongjungkim <plznw4me@naver.com>
2018-04-23 12:51:52 +0900

    add TFRecord Kaggle

--- my_data.py
+++ my_data.py
@@ -13,10 +13,10 @@ test_tfrecords = [test_normal_tfrecord , test_abnormal_tfrecord]
 
 
 if '__main__' == __name__:
-    images , labels , fnames=Dataprovider.Dataprovider.reconstruct_tfrecord_rawdata(test_normal_tfrecord)
-    print np.shape(images[0])
-    print labels
-    print fnames
+    #images , labels , fnames=Dataprovider.Dataprovider.reconstruct_tfrecord_rawdata(test_normal_tfrecord)
+    #print np.shape(images[0])
+    #print labels
+    #print fnames
 
 
     images, labels, filenames = Dataprovider.Dataprovider.get_shuffled_batch(tfrecord_paths=train_tfrecords,
