0d55f9156efd6af305544c57bea92982e8fa71b6
seongjungkim <plznw4me@naver.com>
2018-04-23 12:36:52 +0900

    add TFRecord Kaggle

--- my_data.py
+++ my_data.py
@@ -14,7 +14,7 @@ test_tfrecords = [test_normal_tfrecords , test_abnormal_tfrecords]
 
 if '__main__' == __name__:
     images, labels, filenames = Dataprovider.Dataprovider.get_shuffled_batch(tfrecord_paths=train_tfrecords,
-                                                                             batch_size=10)
+                                                                             batch_size=10 , resize=(300,300) , num_epoch=10)
 
     sess=tf.Session()
     init = tf.group(tf.global_variables_initializer() , tf.local_variables_initializer())
