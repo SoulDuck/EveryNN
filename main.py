9a1fb328b522678843ffc2a8a8b874a5a97643e5
seongjungkim <plznw4me@naver.com>
2018-04-25 21:56:10 +0900

    add augmentation

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ tester=Tester(recorder)
 
 global_step=0
 for i in range(10000):
-    normal_acc, normal_loss, normal_preds=tester.validate_tfrecord(my_data.test_tfrecords , None , None , global_step )
+    val_acc, val_loss, val_preds=tester.validate_tfrecord(my_data.test_tfrecords , None , None , global_step )
     #abnormal_acc, abnormal_loss, abnormal_preds= tester.validate_tfrecord(my_data.test_abnormal_tfrecord, None, None ,global_step )
-
     print ''
-    print 'normal Acc : {} | normal Loss : {}'.format(normal_acc , normal_loss)
+    print 'Validation Acc : {} | Loss : {}'.format(val_acc, val_loss)
     #print 'abnormal Acc : {} | abnormal Loss : {}'.format(abnormal_acc, abnormal_loss)
     #print 'Acc : {} Loss : {}'.format((normal_acc+abnormal_acc)/2. , (normal_loss+abnormal_loss)/2.)
 
