a308a5c8011e33e3578c9d2374bfa7c301a1e73e
seongjungkim <plznw4me@naver.com>
2018-05-28 19:03:34 +0900

    + add L2 apply msg

--- Trainer.py
+++ Trainer.py
@@ -37,6 +37,9 @@ class Trainer(DNN):
             """ #### Traininig  ### """
             train_fetches = [self.train_op, self.accuracy_op, self.cost_op , self.lr_op]
             batch_xs , batch_ys=self.sess.run([self.dataprovider.batch_xs ,self.dataprovider.batch_ys])
+            print np.shape(batch_xs)
+            print batch_ys
+
 
             #utils.plot_images(batch_xs)
             if np.max(batch_xs) > 1:
