3071e34544c32e09a1823cef795dec50aad8cae1
seongjungkim <plznw4me@naver.com>
2018-05-29 01:31:54 +0900

    (resize , resize) --> resize

--- aug.py
+++ aug.py
@@ -53,10 +53,11 @@ def aug_lv0(image_ , is_training , image_size):
 
     def aug_with_train(image, image_size):
         ch=int(image.get_shape()[-1])
-        print ch
+
 
         pad_size = int(image_size * 0.1)
         image = tf.image.resize_image_with_crop_or_pad(image, image_size+pad_size , image_size+pad_size )
+        print image
         image = tf.random_crop(image, [image_size, image_size, ch])
         image = tf.image.random_flip_left_right(image)
         image = tf.image.random_flip_up_down(image)
