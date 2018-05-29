9148230078361b0aacc24afa4fad894d84c2e62e
seongjungkim <plznw4me@naver.com>
2018-05-29 10:19:39 +0900

    fix some code --> fix model save part

--- aug.py
+++ aug.py
@@ -52,7 +52,6 @@ def histo_equalized(img):
 def aug_lv0(image_ , is_training , crop_h , crop_w):
 
     def aug_with_train(image, crop_h , crop_w):
-        print image.get_shape()[1:]
         img_h,img_w,ch=map(int , image.get_shape()[1:])
 
         pad_w = int(img_h * 0.1)
