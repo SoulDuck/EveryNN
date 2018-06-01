7c1446f58c6275b86868c5fe106a7a52b9c8cb2d
seongjungkim <plznw4me@naver.com>
2018-06-01 16:39:01 +0900

    add plotROC

--- Tester.py
+++ Tester.py
@@ -217,11 +217,6 @@ class Tester(DNN):
         return masked_img
 
 
-
-
-
-
-
     def eval(self, model_path, test_imgs, test_labs, batch_size , actmap_dir):
         self._reconstruct_model(model_path)
         self.validate(test_imgs , test_labs , batch_size , step=0 , save_model = False)
@@ -229,6 +224,60 @@ class Tester(DNN):
             cams = self._extract_actmap(test_imgs)
             print np.shape(cams)
         return self.pred_all , self.acc , self.loss
+
+    def plotROC(predStrength, labels):
+        debug_flag = False
+        assert np.ndim(predStrength) == np.ndim(labels)
+        if np.ndim(predStrength) == 2:
+            predStrength = np.argmax(predStrength, axis=1)
+            labels = np.argmax(labels, axis=1)
+
+        # how to input?
+
+        cursor = (1.0, 1.0)  # initial cursor
+        ySum = 0.0  # for AUC curve
+        n_pos = np.sum(np.array(labels) == 1)
+        n_neg = len(labels) - n_pos
+        y_step = 1 / float(n_pos)
+        x_step = 1 / float(n_neg)
+        n_est_pos = 0
+        sortedIndices = np.argsort(predStrength, axis=0)
+        fig = plt.figure()
+        fig.clf()
+        ax = plt.subplot(1, 1, 1)
+        if __debug__ == debug_flag:
+            print 'labels', labels[:10]
+            print 'predStrength', predStrength.T[:10]
+            print 'sortedIndices', sortedIndices.T[:10]
+            print  sortedIndices.tolist()[:10]
+        for ind in sortedIndices.tolist():
+            print ind
+            if labels[ind] == 1.0:
+                DelX = 0;
+                DelY = y_step
+            else:
+                DelX = x_step;
+                DelY = 0
+                ySum += cursor[1]
+            ax.plot([cursor[0], cursor[0] - DelX], [cursor[1], cursor[1] - DelY])
+            cursor = (cursor[0] - DelX, cursor[1] - DelY)
+            if __debug__ == debug_flag:
+                print 'label', labels[ind]
+                print 'delX',
+                print 'sortedIndices', sortedIndices.T
+                print 'DelX:', DelX, 'DelY:', DelY
+                print 'cursor[0]-DelX :', cursor[0], 'cursor[1]-DelY :', cursor[1]
+        ax.plot([0, 1], [0, 1], 'b--')
+        plt.xlabel('False Positive Rate');
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC curve for Fundus Classification System')
+        ax.axis([0, 1, 0, 1])
+        if __debug__ == debug_flag:
+            print '# of True :', n_pos
+            print '# of False :', n_neg
+        plt.savefig('roc.png')
+        # plt.show()
+        print 'The Area Under Curve is :', ySum * x_step
 
 if __name__ =='__main__':
     imgs = []
