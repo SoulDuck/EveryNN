15a517921c6225109cbe8ae7d02f8981102224c4
kimseongjung <plznw4me@naver.com>
2018-06-08 15:50:48 +0900

    add project 9 data code

--- my_data.py
+++ my_data.py
@@ -9,12 +9,7 @@ import random
 
 #항상 이런형태로 train , test tfrecords 형태로 해야한다.
 
-train_tfrecord_path = './my_data/project6/val_0_75_test_75_225/train.tfrecord'
-test_tfrecord_path = './my_data/project6/val_0_75_test_75_225/test.tfrecord'
-val_tfrecord_path = './my_data/project6/val_0_75_test_75_225/val.tfrecord'
-
-
-def make_tfrecord(tfrecord_path, resize , normal_imgs , abnormal_imgs , n_nor , n_abnor):
+def make_tfrecord(tfrecord_path, resize , normal_imgs , abnormal_imgs ,*args ):
     """
     img source 에는 두가지 형태로 존재합니다 . str type 의 path 와
     numpy 형태의 list 입니다.
@@ -24,15 +19,9 @@ def make_tfrecord(tfrecord_path, resize , normal_imgs , abnormal_imgs , n_nor , 
     :param labels: 3.g) [1,1,1,1,1,0,0,0,0]
     :return:
     """
-    debug_flag_lv0 = False
-    debug_flag_lv1 = False
-    if __debug__ == debug_flag_lv0:
-        print 'debug start | batch.py | class : tfrecord_batch | make_tfrecord_rawdata'
-
     if os.path.exists(tfrecord_path):
         print tfrecord_path + 'is exists'
         return
-
     def _bytes_feature(value):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
@@ -40,38 +29,34 @@ def make_tfrecord(tfrecord_path, resize , normal_imgs , abnormal_imgs , n_nor , 
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
     writer = tf.python_io.TFRecordWriter(tfrecord_path)
-    n_abnormal = len(abnormal_imgs)
-    n_normal = len(normal_imgs)
-    n_train = len(normal_imgs)*2
 
-    NORMAL =0
-    ABNORMAL = 1
-
-    normal_count=0
-    abnormal_count =0
     flag=True
+    n_total =0
+    counts = []
+    for i,arg in enumerate(args):
+        print 'Label :{} , # : {} '.format(i , arg[i])
+        n_total += arg[0]
+        counts.append(0)
+
     while(flag):
-        label=random.randint(0, 1)
-        if label == NORMAL and normal_count < n_nor:
-            np_img=normal_imgs[normal_count]
-            normal_count +=1
-            ind = normal_count
-
-        elif label == ABNORMAL and abnormal_count < n_abnor: # *** Normal n imgs and Abnormal n imgs
-            np_img = abnormal_imgs[abnormal_count % n_abnormal]
-            abnormal_count +=1
-            ind = abnormal_count
-
-        elif normal_count + abnormal_count ==  n_nor + n_abnor :
-            print '\t normal_count : {}'.format(normal_count)
-            print '\t abnormal count {}'.format(abnormal_count)
+        label=random.randint(len(args))
+        n_max = args[label][0]
+        if counts[label] < n_max:
+            imgs = args[label][1]
+            n_imgs = len(args[label][1])
+            ind = counts[label] % n_imgs
+            np_img = imgs[ind]
+            counts[label] += 1
+        elif np.sum(np.asarray(counts)) ==  n_total:
+            for i, count in enumerate(counts):
+                print 'Label : {} , # : {} '.format(i, count )
             flag = False
         else:
             continue;
 
         height, width = np.shape(np_img)[:2]
 
-        msg = '\r-Progress : {0}'.format(str(normal_count + abnormal_count) + '/' + str(n_nor + n_abnor))
+        msg = '\r-Progress : {0}'.format(str(np.sum(np.asarray(counts))) + '/' + str(n_total))
         sys.stdout.write(msg)
         sys.stdout.flush()
         if not resize is None:
@@ -87,7 +72,15 @@ def make_tfrecord(tfrecord_path, resize , normal_imgs , abnormal_imgs , n_nor , 
         writer.write(example.SerializeToString())
     writer.close()
 
+# project 6
+train_tfrecord_path = './my_data/project6/val_0_75_test_75_225/train.tfrecord'
+test_tfrecord_path = './my_data/project6/val_0_75_test_75_225/test.tfrecord'
+val_tfrecord_path = './my_data/project6/val_0_75_test_75_225/val.tfrecord'
+
+# project 9
+
 if '__main__' == __name__:
+
     # project 5
     """
     cac_dir = '../fundus_data/cacs/imgSize_350/nor_0_10_abnor_300_inf/1/seoulfundus'
@@ -98,9 +91,26 @@ if '__main__' == __name__:
     """
 
     # project 6
-
-
+    """
     cac_dir = '/home/mediwhale/fundus_harddisk/merged_CACS_350/1year/Numpy_Images/val_0_75_test_75_225'
+    nor_test_imgs=np.load(os.path.join(cac_dir , 'normal_test.npy'))
+    abnor_test_imgs = np.load(os.path.join(cac_dir, 'abnormal_test.npy'))
+
+    nor_train_imgs=np.load(os.path.join(cac_dir , 'normal_train.npy'))
+    abnor_train_imgs = np.load(os.path.join(cac_dir, 'abnormal_train.npy'))
+
+    nor_val_imgs = np.load(os.path.join(cac_dir, 'normal_val.npy'))
+    abnor_val_imgs = np.load(os.path.join(cac_dir, 'abnormal_val.npy'))
+
+    # Train 이미지 수는 normal Image 와 똑같이 만든다
+    make_tfrecord(train_tfrecord_path, None, nor_train_imgs, abnor_train_imgs , len(nor_train_imgs) , len(nor_train_imgs))
+    make_tfrecord(test_tfrecord_path,None , nor_test_imgs , abnor_test_imgs , len(nor_test_imgs) , len(abnor_test_imgs)) # Train TF Recorder
+    make_tfrecord(val_tfrecord_path, None, nor_val_imgs, abnor_val_imgs, len(nor_val_imgs) , len(abnor_val_imgs)) # Test TF Recorder
+    """
+
+    #project 9
+    cac_dir = '/home/mediwhale/fundus_harddisk/merged_CACS_350/1year/Numpy_Images/val_0_75_test_75_225'
+
     nor_test_imgs=np.load(os.path.join(cac_dir , 'normal_test.npy'))
     abnor_test_imgs = np.load(os.path.join(cac_dir, 'abnormal_test.npy'))
 
--- tmp.py
+++ tmp.py
@@ -1,14 +1,8 @@
-import os
-import numpy as np
-from PIL import Image
-import Dataprovider
-path = '/Volumes/Seagate Backup Plus Drive/IMAC/project6/val.tfrecord'
-path = '/Volumes/Seagate Backup Plus Drive/IMAC/project6/test.tfrecord'
-imgs ,labs , fnames = Dataprovider.Dataprovider.reconstruct_tfrecord_rawdata(path , resize=None)
+def _fn(*args):
+    for arg in args:
+        print arg
+        print len(args)
+_fn((1,3) , (2,4))
 
-print labs
-labs=np.asarray(labs)
-indices=[labs==0]
-print labs[indices]
-indices=np.where([labs==1])[1]
-print labs[indices]
+
+print 30%100
