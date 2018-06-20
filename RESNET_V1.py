f3de640bd9f2523277ef413213f4ce3479e23a21
seongjungkim <plznw4me@naver.com>
2018-06-20 18:43:17 +0900

    kaggle fundus

--- RESNET_V1.py
+++ RESNET_V1.py
@@ -72,10 +72,15 @@ class RESNET_V1(DNN):
 
         with tf.variable_scope('stem'):
             # conv filters out = 64
+
             layer = self.convolution2d('conv_0', out_ch= 32,  x=self.input, k=7, s=2)
             layer = self.batch_norm_layer(layer, phase_train = self.is_training, scope_bn='bn')
+            if int(layer.get_shape()[1]) > 500:
+                layer = self.convolution2d('conv_1', out_ch= 32,  x=layer, k=3, s=2)
+                layer = self.batch_norm_layer(layer, phase_train = self.is_training, scope_bn='bn')
+
+
             #layer = self.activation(layer)
-            # BN을 activation 후에 하는게 좋은지 앞에서 하는게 좋은지는 토론중이다. 난 개인적으로 weight 을 앞에다 하는게 성능을 높일거라 생각한다.
         for box_idx in range(len(self.n_filters_per_box)):
             print '#######   box_{}  ########'.format(box_idx)
             with tf.variable_scope('box_{}'.format(box_idx)):
