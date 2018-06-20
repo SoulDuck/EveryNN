db3fd4b2d609e18cbc78afd2d8b219d7f042b37e
seongjungkim <plznw4me@naver.com>
2018-06-20 15:51:09 +0900

    kaggle fundus

--- main_eval.py
+++ main_eval.py
@@ -6,9 +6,8 @@ import os
 dirpath = '/home/mediwhale-5/PythonProjects/everyNN/my_data/project10'
 test_data = os.path.join(dirpath ,'test_nor_0_10_abnor_10_inf.tfrecord')
 val_data = os.path.join(dirpath ,'val_nor_0_10_abnor_10_inf.tfrecord')
+#restore_model = '/home/mediwhale-5/PythonProjects/everyNN/models/resnet_18/4'
 restore_model = '/Users/seongjungkim/PycharmProjects/everyNN/models/best_model/0/model-26730'
-
-restore_model = '/home/mediwhale-5/PythonProjects/everyNN/models/resnet_18/4'
 tester=Tester.Tester(None)
 
 tester._reconstruct_model(restore_model)
@@ -16,10 +15,8 @@ tester._reconstruct_model(restore_model)
 test_imgs  , test_labs , test_fs = Dataprovider.reconstruct_tfrecord_rawdata(test_data , None )
 val_imgs ,val_labs , val_fs = Dataprovider.reconstruct_tfrecord_rawdata(val_data , None)
 
-print np.shape(test_imgs)
-print np.shape(val_imgs)
-
 tester.validate(val_imgs ,val_labs , 60 , 0 , None )
+
 
 
 
