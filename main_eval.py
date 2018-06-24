600ad460bffd31808ca73879421ae1e02a06d6c0
kimseongjung <plznw4me@naver.com>
2018-06-24 14:56:51 +0900

    add ensemble models

--- main_eval.py
+++ main_eval.py
@@ -75,7 +75,7 @@ print tester.acc_by_labels
 for pred in tester.pred_all:
     print pred
 test_cls=np.argmax(test_labs , axis=1)
-predStrength=np.asarray(tester.pred_all)[:,0]
+predStrength=np.asarray(tester.pred_all)[:,1]
 tester.plotROC(predStrength=predStrength , labels= test_cls ,  prefix='CAC fundus classifier' , savepath='tmp.png')
 """
 for pred in tester.pred_all:
