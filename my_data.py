674999f92ceb5b680783fd95805ed0fe7b9ae744
kimseongjung <plznw4me@naver.com>
2018-06-26 14:53:23 +0900

    add ensemble models

--- my_data.py
+++ my_data.py
@@ -107,22 +107,24 @@ train_tfrecord_path = './my_data/project10/train_0_9_10_55_56_inf.tfrecord'
 test_tfrecord_path = './my_data/project10/test_0_9_10_55_56_inf.tfrecord'
 val_tfrecord_path = './my_data/project10/val_0_9_10_55_56_inf.tfrecord'
 
-
 # Data ID 0100-0000003-019
 train_tfrecord_path = '../cac_regressor/train_0_10_11_inf.tfrecord'
 val_tfrecord_path = '../cac_regressor/val_0_10_11_inf.tfrecord'
 test_tfrecord_path = '../cac_regressor/test_0_10_11_inf.tfrecord'
 
 
-# Data ID 0100-0000003-022
-train_tfrecord_path = '../cac_regressor/0100-0000003-022/train_0_10_11_inf.tfrecord'
-val_tfrecord_path = '../cac_regressor/0100-0000003-022/val_0_10_11_inf.tfrecord'
-test_tfrecord_path = '../cac_regressor/0100-0000003-022/test_0_10_11_inf.tfrecord'
+
 
 # Data ID 0100-0000003-020
 train_tfrecord_path = '../cac_regressor/0100-0000003-020/train_0_30_31_inf.tfrecord'
 val_tfrecord_path = '../cac_regressor/0100-0000003-020/val_0_30_31_inf.tfrecord'
 test_tfrecord_path = '../cac_regressor/0100-0000003-020/test_0_30_31_inf.tfrecord'
+
+
+# Data ID 0100-0000003-022
+train_tfrecord_path = '../cac_regressor/0100-0000003-022/train_0_10_11_inf.tfrecord'
+val_tfrecord_path = '../cac_regressor/0100-0000003-022/val_0_10_11_inf.tfrecord'
+test_tfrecord_path = '../cac_regressor/0100-0000003-022/test_0_10_11_inf.tfrecord'
 
 
 
