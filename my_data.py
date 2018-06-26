acf2918927f23a6ec9afc02f9b10d943f5514212
seongjungkim <plznw4me@naver.com>
2018-06-26 12:51:52 +0900

    applyu projection

--- my_data.py
+++ my_data.py
@@ -113,16 +113,17 @@ train_tfrecord_path = '../cac_regressor/train_0_10_11_inf.tfrecord'
 val_tfrecord_path = '../cac_regressor/val_0_10_11_inf.tfrecord'
 test_tfrecord_path = '../cac_regressor/test_0_10_11_inf.tfrecord'
 
-# Data ID 0100-0000003-020
-train_tfrecord_path = '../cac_regressor/0100-0000003-020/train_0_30_31_inf.tfrecord'
-val_tfrecord_path = '../cac_regressor/0100-0000003-020/val_0_30_31_inf.tfrecord'
-test_tfrecord_path = '../cac_regressor/0100-0000003-020/test_0_30_31_inf.tfrecord'
-
 
 # Data ID 0100-0000003-022
 train_tfrecord_path = '../cac_regressor/0100-0000003-022/train_0_10_11_inf.tfrecord'
 val_tfrecord_path = '../cac_regressor/0100-0000003-022/val_0_10_11_inf.tfrecord'
 test_tfrecord_path = '../cac_regressor/0100-0000003-022/test_0_10_11_inf.tfrecord'
+
+# Data ID 0100-0000003-020
+train_tfrecord_path = '../cac_regressor/0100-0000003-020/train_0_30_31_inf.tfrecord'
+val_tfrecord_path = '../cac_regressor/0100-0000003-020/val_0_30_31_inf.tfrecord'
+test_tfrecord_path = '../cac_regressor/0100-0000003-020/test_0_30_31_inf.tfrecord'
+
 
 
 if '__main__' == __name__:
