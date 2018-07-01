cb52fd91c835f9e207e19f35abab54761fde6e63
kimseongjung <plznw4me@naver.com>
2018-07-01 21:53:03 +0900

    add ensemble models

--- main_eval.py
+++ main_eval.py
@@ -16,7 +16,7 @@ dirpath = './my_data/0100-0000003-019'
 
 
 restore_model  = '/home/mediwhale-5/PythonProjects/everyNN/models/resnet_18/10/model-26631'
-dirpath = '/home/mediwhale-5/PythonProjects/cac_regressor/0100-0000003-023/0100-0000003-022'
+dirpath = '/home/mediwhale-5/PythonProjects/cac_regressor/'
 
 test_data = os.path.join(dirpath ,'test_0_10_11_inf.tfrecord')
 val_data = os.path.join(dirpath ,'val_0_10_11_inf.tfrecord')
