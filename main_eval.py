983781bfea61f33e9dc5414bebb6c2798a0451d7
kimseongjung <plznw4me@naver.com>
2018-07-01 22:30:26 +0900

    add ensemble models

--- main_eval.py
+++ main_eval.py
@@ -14,10 +14,13 @@ dirpath = '/home/mediwhale-5/PythonProjects/everyNN/my_data/project10'
 restore_model = './models/best_models/exam_id_000000065/model-37719'
 dirpath = './my_data/0100-0000003-019'
 
-
 restore_model  = '/home/mediwhale-5/PythonProjects/everyNN/models/resnet_18/10/model-26631'
 dirpath = '/home/mediwhale-5/PythonProjects/cac_regressor/0100-0000003-023/0100-0000003-022'
 
+
+restore_model  = '/home/mediwhale/PycharmProjects/everyNN/models/resnet_34/4/model-29700'
+#dirpath = '/home/mediwhale-5/PythonProjects/cac_regressor/0100-0000003-023/0100-0000003-022'
+"""
 test_data = os.path.join(dirpath ,'test_0_10_11_inf.tfrecord')
 val_data = os.path.join(dirpath ,'val_0_10_11_inf.tfrecord')
 
@@ -30,7 +33,7 @@ val_labs=cls2onehot(val_labs ,2)
 test_labs=cls2onehot(test_labs ,2)
 
 
-"""
+
 for i,img in enumerate(val_imgs):
     plt.imshow(test_imgs[0])
     plt.show()
@@ -38,7 +41,7 @@ for i,img in enumerate(val_imgs):
 
 exit()
 """
-"""
+
 
 paths_0=glob.glob('./images/0100-0000003-019_label_0/*.png')
 paths_0=sorted(paths_0)
@@ -66,7 +69,7 @@ test_imgs=np.asarray(imgs)
 assert len(test_labs) == len(test_imgs)
 print np.shape(test_imgs)
 
-"""
+
 
 tester=Tester.Tester(None)
 tester._reconstruct_model(restore_model)
@@ -82,17 +85,16 @@ print tester.acc_by_labels
 for pred in tester.pred_all:
     print pred
 
+
 test_cls=np.argmax(test_labs , axis=1)
 predStrength=np.asarray(tester.pred_all)[:,1]
 tester.plotROC(predStrength=predStrength , labels= test_cls ,  prefix='CAC fundus classifier' , savepath='tmp.png')
-
 """
+
 for pred in tester.pred_all:
     print pred
 for lab in val_labs:
     print lab
-"""
-"""
 tester.ensemble(test_imgs, test_labs, 60, './models/best_models/0_from_5555/model-37719', './models/best_models/0_from_5566/model-14751',
                 './models/best_models/0_from_5571/model-45144', './models/best_models/1_from_5555/model-25839',
                 './models/best_models/1_from_5571/model-64548')
