f0393c7d75738e99d01670fcd1789a1bf0b0dd23
kimseongjung <plznw4me@naver.com>
2018-07-07 18:22:34 +0900

    sensitivity , specificity`

--- Recorder.py
+++ Recorder.py
@@ -36,38 +36,38 @@ class Recorder(DNN):
         self.summary_writer.add_summary(summary,step)
 
 
-def _make_logfolder(self):
-    log_count =0;
-    while True:
-        logs_root_path='./logs/{}'.format(self.folder_name)
-        try:
-            os.makedirs(logs_root_path)
-        except Exception as e :
-            pass;
-        self.logs_path=os.path.join( logs_root_path , str(log_count))
-        if not os.path.isdir(self.logs_path):
-            os.mkdir(self.logs_path)
-            break;
-        else:
-            log_count+=1
-    print 'folder where logs is saved : {} '.format(self.logs_path)
+    def _make_logfolder(self):
+        log_count =0;
+        while True:
+            logs_root_path='./logs/{}'.format(self.folder_name)
+            try:
+                os.makedirs(logs_root_path)
+            except Exception as e :
+                pass;
+            self.logs_path=os.path.join( logs_root_path , str(log_count))
+            if not os.path.isdir(self.logs_path):
+                os.mkdir(self.logs_path)
+                break;
+            else:
+                log_count+=1
+        print 'folder where logs is saved : {} '.format(self.logs_path)
 
 
 
-def _make_modelfolder(self):
-    model_count =0;
-    while True:
-        models_root_path='./models/{}'.format(self.folder_name)
-        try:
-            os.makedirs(models_root_path)
-        except Exception as e:
-            pass;
-        self.models_path=os.path.join(models_root_path , str(model_count))
+    def _make_modelfolder(self):
+        model_count =0;
+        while True:
+            models_root_path='./models/{}'.format(self.folder_name)
+            try:
+                os.makedirs(models_root_path)
+            except Exception as e:
+                pass;
+            self.models_path=os.path.join(models_root_path , str(model_count))
 
-        if not os.path.isdir(self.models_path):
-            os.mkdir(self.models_path)
-            break; #
-        else:
-            model_count+=1
+            if not os.path.isdir(self.models_path):
+                os.mkdir(self.models_path)
+                break; #
+            else:
+                model_count+=1
 
-    print 'folder where models is saved : {} '.format(self.models_path)
+        print 'folder where models is saved : {} '.format(self.models_path)
