e348ff3f0ad29198a74aba11bfe7d222f73b6b74
kimseongjung <plznw4me@naver.com>
2018-09-27 16:52:54 +0900

    add Densenet

--- wally.py
+++ wally.py
@@ -1,6 +1,6 @@
-train_tfrecord_path = '/mnt/Find_Wally/Wally_ver3/train.tfrecord'
-test_tfrecord_path = '/mnt/Find_Wally/Wally_ver3/test.tfrecord'
-val_tfrecord_path = '/mnt/Find_Wally/Wally_ver3//val.tfrecord'
+train_tfrecord_path = '/mnt/Find_Wally/Wally_ver3/tfrecords/train.tfrecord'
+test_tfrecord_path = '/mnt/Find_Wally/Wally_ver3/tfrecords/test.tfrecord'
+val_tfrecord_path = '/mnt/Find_Wally/Wally_ver3/tfrecords/val.tfrecord'
 
 
 """
