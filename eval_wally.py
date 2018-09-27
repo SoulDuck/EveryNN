d67910ac900991b04cc38c147aa769d16d2a22fe
kimseongjung <plznw4me@naver.com>
2018-09-27 17:47:06 +0900

    add Densenet

--- eval_wally.py
+++ eval_wally.py
@@ -19,7 +19,7 @@ best           second
 
 """
 
-test_imgs = np.load('./wally_data/3.npy')
+test_imgs = np.load('./wally_data/2.npy')
 test_labs=[0]*len(test_imgs)
 
 test_labs=cls2onehot(test_labs ,2 )
@@ -29,7 +29,7 @@ test_imgs = test_imgs/255.
 tester.validate(test_imgs , test_labs, 60 ,0 ,False)
 
 indices = np.where([np.asarray(tester.pred_all)[:,0] > 0.5])[1]
+"""
 wally_imgs = test_imgs[indices]
-
 np.save('wally_img.npy' , wally_imgs)
-
+"""
