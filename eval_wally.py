49b7a8b6d2e97abb76df9ffecba05456de739960
kimseongjung <plznw4me@naver.com>
2018-10-02 02:18:42 +0900

    add Densenet

--- eval_wally.py
+++ eval_wally.py
@@ -26,7 +26,7 @@ test_labs=[0]*len(test_imgs)
 test_labs=cls2onehot(test_labs ,2 )
 
 
-test_imgs = test_imgs
+test_imgs = test_imgs/255.
 tester.validate(test_imgs , test_labs, 60 ,0 ,False)
 indices = np.where([np.asarray(tester.pred_all)[:,0] > 0.5])[1]
 print indices
