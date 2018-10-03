857bbac74a3278761b9e4e69890066bb3f8cde7f
kimseongjung <plznw4me@naver.com>
2018-10-03 15:57:22 +0900

    add Densenet

--- eval_wally.py
+++ eval_wally.py
@@ -7,7 +7,7 @@ def cls2onehot(cls , depth):
     for i,c in enumerate(cls):
         labs[i,c]=1
     return labs
-restore_model  = './models/vgg_11/18/model-564'
+restore_model  = './models/vgg_11/18/model-128'
 tester=Tester.Tester(None)
 tester._reconstruct_model(restore_model)
 
